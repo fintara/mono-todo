9a01190aa1831b25194cf39fd17847fb3ffaba3b
Tsvetan Ovedenski <dev@tsovedenski.com>
2020-03-21 17:27:48 +0100

    [frontend] Add demo notice

--- todo-frontend/src/components/AppHeader/AppHeader.tsx
+++ todo-frontend/src/components/AppHeader/AppHeader.tsx
@@ -3,10 +3,10 @@ import styles from "./styles.module.scss"
 import Container from "../../ui/Container"
 import { useApp } from "../../app"
 import { User } from "../../app/auth/types"
-import { Button } from "@blueprintjs/core"
+import { Button, Callout } from "@blueprintjs/core"
 
 const UserInformation: React.FC<{ user: User, onLogout: () => void }> = ({ user, onLogout }) =>
-  <div>
+  <div className={styles.userInformation}>
     <span className={styles.text}>Hello, {user.name}!</span>
     <Button small={true} onClick={onLogout} rightIcon="log-out">Log out</Button>
   </div>
@@ -16,6 +16,7 @@ const AppHeader: React.FC = () => {
 
   return (
     <Container className={styles.container}>
+      {process.env.REACT_APP_IS_DEMO === "true" && <Callout intent="warning" className={styles.callout}>This is a demo. Data persistence is not guaranteed!</Callout>}
       {state.auth.user && <UserInformation user={state.auth.user} onLogout={actions.auth.logout} />}
     </Container>
   )
